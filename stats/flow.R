576d840d7047998afeece7df87f6e07971563ae8
jfrey <nomhad@gmail.com>
2016-08-08 20:16:49 +0200

    Merging eduflow and data

--- stats/flow.R
+++ stats/flow.R
@@ -11,6 +11,25 @@ wilcox.test(music$score, nomusic$score, paired=TRUE)
 
 wilcox.test(music$score, nomusic$score)
 
-boxplot(music$score, nomusic$score)
+#boxplot(music$score, nomusic$score)
 
-subset(eduflow_data, music == 1)
+## perfs
+
+# loading data and adding a column for eduflow
+perf_data = read.csv("data.csv")
+perf_data$eduflow = -1
+
+ids = unique(perf_data$list_id)
+
+
+for (id in ids) {
+  cat("\n\nSujet ID:", id, "\n")
+  mus_edu_score = eduflow_data[eduflow_data$ID == id & eduflow_data$musique == 1,]$score
+  cat("music score", mus_edu_score, "\n")
+  perf_data[perf_data$list_id == id & perf_data$list_condition == "mus"]$eduflow = mus_edu_score
+
+  nomus_edu_score = eduflow_data[eduflow_data$ID == id & eduflow_data$musique == 0,]$score
+  cat("nomusic score", nomus_edu_score, "\n")
+  perf_data[perf_data$list_id == id & perf_data$list_condition == "nomus"]$eduflow = nomus_edu_score
+
+}
