5552eb5571069de357bba31cdd064b58ac31d554
jfrey <nomhad@gmail.com>
2016-08-09 01:59:07 +0200

    trying 3D dimenison

--- stats/flow.R
+++ stats/flow.R
@@ -3,6 +3,8 @@
 eduflow_data = read.csv("Eduflow2_stats.csv")  # read csv file 
 
 eduflow_data$score = as.numeric(sub(",", ".", eduflow_data$score, fixed = TRUE))
+eduflow_data$score3D = as.numeric(sub(",", ".", eduflow_data$score3D, fixed = TRUE))
+
 
 music = eduflow_data[eduflow_data$musique == 1,]
 nomusic = eduflow_data[eduflow_data$musique == 0,]
@@ -18,6 +20,7 @@ wilcox.test(music$score, nomusic$score)
 # loading data and adding a column for eduflow
 perf_data = read.csv("data.csv")
 perf_data$eduflow = -1
+perf_data$eduflow3D = -1
 
 # for overall score
 perf_data$total = -1
@@ -35,18 +38,19 @@ for (id in ids) {
   cat("nomusic score", nomus_edu_score, "\n")
   perf_data[perf_data$list_id == id & perf_data$list_cond == "nomus",]$eduflow = nomus_edu_score
   
-  score_L_mus = mean(perf_data[perf_data$list_id == id & perf_data$list_cond == "nomus" & perf_data$list_direction == 0,]$list_score)
-  score_R_mus = mean(perf_data[perf_data$list_id == id & perf_data$list_cond == "nomus" & perf_data$list_direction == 1,]$list_score)
+  # 3D version
+  mus_edu_score3D = eduflow_data[eduflow_data$ID == id & eduflow_data$musique == 1,]$score3D
+  cat("music score3D", mus_edu_score3D, "\n")
+  perf_data[perf_data$list_id == id & perf_data$list_cond == "mus",]$eduflow3D = mus_edu_score3D
 
+  nomus_edu_score3D = eduflow_data[eduflow_data$ID == id & eduflow_data$musique == 0,]$score3D
+  cat("nomusic score3D", nomus_edu_score3D, "\n")
+  perf_data[perf_data$list_id == id & perf_data$list_cond == "nomus",]$eduflow3D = nomus_edu_score3D
 }
 
-aggregate(perf_data, list("list_id"), mean)
 
-
-data_mean = aggregate( cbind(list_class, list_score, eduflow) ~ list_id + list_cond + list_direction, data = perf_data, FUN = mean)
+data_mean = aggregate( cbind(list_class, list_score, eduflow, eduflow3D) ~ list_id + list_cond + list_direction, data = perf_data, FUN = mean)
  
-data_mean[data_mean$list_direction == 0,]
-
 # how score and classifier output relate
 plot(data_mean$list_class, data_mean$list_score)
 
@@ -57,34 +61,85 @@ data_mean_bis[data_mean_bis$list_direction == 0,]$list_class = data_mean_bis[dat
 # score/classifier, no matter class
 plot(data_mean_bis$list_class, data_mean_bis$list_score)
 
-
 # average across left/right
-data_across = aggregate( cbind(list_class, list_score, eduflow) ~ list_id + list_cond, data = data_mean_bis, FUN = mean)
+data_across = aggregate( cbind(list_class, list_score, eduflow, eduflow3D) ~ list_id + list_cond, data = data_mean_bis, FUN = mean)
 
 # try to sense some sense
 plot(data_across$list_class, data_across$list_score)
 plot(data_across$list_score, data_across$eduflow)
 plot(data_across$list_class, data_across$eduflow)
 
+## correlations
+
+library(Hmisc)
+
+# linear
+rcorr(data_across$list_score, data_across$eduflow , type="pearson") 
+rcorr(data_across$list_class, data_across$eduflow , type="pearson") 
+
+# non linear
+rcorr(data_across$list_score, data_across$eduflow , type="spearman") 
+rcorr(data_across$list_class, data_across$eduflow , type="spearman") 
+
+# best: pearson, class/eduflow
+
+rcorr(data_across$list_class, data_across$eduflow , type="pearson") 
+plot(data_across$list_class, data_across$eduflow)
+abline(lm(data_across$eduflow ~ data_across$list_class))
+
+linear_regres = lm(data_across$eduflow ~ data_across$list_class)
+linear_coeff = coefficients(linear_regres)
+summary(linear_coeff)
+fitted(linear_regres)
+abline(4.871517,1.628709)
+
+# equation
+# y = 1.628709 * x + 4.871517
+
+x = (7 - 4.871517) / 1.628709
+
+## left / right
+
+data_split = data_mean_bis
+data_split$class_left = data_split$list_class
+data_split$class_right = data_split$list_class
+data_split[data_split$list_direction == 0,]$class_right = data_split[data_split$list_direction == 1,]$class_right
+data_split = data_split[data_split$list_direction == 0,]
+data_split$class_both = data_split$class_left + data_split$class_right
+
+lm(data_split$eduflow ~ data_split$class_left + data_split$class_right)
+
+lm(data_split$eduflow ~ data_split$class_both)
+
+# dist max is sqrt(8)
+data_split$dist = sqrt(8) - sqrt((1 - data_split$class_left)^2 + (1 - data_split$class_right)^2 )
+
+
+plot(data_split$dist, data_split$eduflow)
+
+rcorr(data_split$eduflow,data_split$dist, type="pearson")
+lm(data_split$eduflow ~ data_split$dist )
+abline(3.271, 1.211)
+
+# y = 1.211 * x + 4.274
+
+x = (7 - 3.271) / 1.211
+
+#3D version
+
+##
 
 
 data_left = data_mean_bis[data_mean_bis$list_direction == 0,]
 plot(data_left$list_score, data_left$eduflow)
 plot(data_left$list_class, data_left$eduflow)
+lm(data_left$eduflow ~ data_left$list_class)
 
 data_right = data_mean_bis[data_mean_bis$list_direction == 1,]
 plot(data_right$list_score, data_right$eduflow)
 plot(data_right$list_class, data_right$eduflow)
+lm(data_right$eduflow ~ data_right$list_class)
+
 
 plot(data_across$list_score, data_across$eduflow)
 
-
-
-  aggregate( list_class ~ list_id + list_cond, data = perf_data, FUN = mean)
-
-## correlations
-
-library(Hmisc)
-
-
-rcorr(perf_data$score , type="pearson") 
