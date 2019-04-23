7a4111a4467ed3b7e03531b98fb19bffef4a2f86
jfrey <nomhad@gmail.com>
2019-04-23 12:19:12 +0200

    update format for calib

--- concatenate.R
+++ concatenate.R
@@ -6,7 +6,7 @@ rm(list=ls())
 # GDF files are read from here. In this version, only expect one participant, whose files during calibration are named motor-imagery-csp-1-acquisition-[2016.08.04-15.55.00].gdf
 datasetFolder<-"./signals/";
 # file format recorded during calibration
-datasetPattern<-"motor-imagery-csp-1-acquisition-.*\\.gdf";
+datasetPattern<-"motor-imagery-csp-1-acquisition-.*_EEG\\.gdf";
 
 # the label and signal CSV files are written here
 outputFolder<-"./signals/";
