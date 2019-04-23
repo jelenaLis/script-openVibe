8d8bb7a4719c13794b300efea280633999f11ecb
jmladeno <jmladeno@ad.inria.fr>
2019-04-23 12:58:17 +0200

    tuning and hotfix before experiment

--- concatenate.R
+++ concatenate.R
@@ -13,7 +13,8 @@ outputFolder<-"./signals/";
 outputFilename<-"concatenated_training.gdf"
 
 g_NoGui<- "--no-gui"; # set to empty "" to see the GUI, good for debugging
-g_Designer<-"ov-designer-2.2";
+#g_Designer<-"ov-designer-2.2";
+g_Designer<-"C:/Program Files/openvibe-2.2.0-64bit/openvibe-designer.cmd"
 g_Scenario<-"concatenate.xml" 
 
 #### should be nothing to modify below this
--- frequencyBandSelection.py
+++ frequencyBandSelection.py
@@ -5,7 +5,7 @@
 
 from __future__ import print_function, division
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 class FrequencyBandSelectionBox(OVBox):
     def __init__(self):
